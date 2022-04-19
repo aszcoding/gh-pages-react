be782ccd602042eb2eb20fb5cddbbc1a60e80107
Ashley Serrano-Ziel <ashley.serranoziel@gmail.com>
2022-04-19 14:41:04 -0600

    Updated homepage

--- src/App.js
+++ src/App.js
@@ -5,9 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          My awesome GitHub Page!
         </p>
         <a
           className="App-link"
